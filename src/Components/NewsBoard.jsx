9c36f619ab8268bb8476760c7954fef0d2eea4a9
Mridul Mani Tripathi <mridulmani350@gmail.com>
2024-12-19 01:05:27 +0530

    added vercel.json file

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -7,9 +7,7 @@ const NewsBoard = ({category}) => {
     const [articles,setArticles] = useState([]);
 
     useEffect(()=>{
-      const url = 'https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=VITE_API_KEY';
-
-      //let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
       fetch(url).then(response=>response.json()).then(data=> setArticles(data.articles));
     
     },[category])
